35de87fb770944e66201abed9e93ad58870d25cf
Blitblank <pmcgeee03@gmail.com>
2025-03-12 13:45:49 -0500

    added basic http req

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { ApiService } from 'src/services/api.service';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+
+	constructor(private apiService: ApiService) {}
+
+	public sendReq() {
+		this.apiService.getData().subscribe(response => {
+			console.log('GET Response:', response);
+		});
+	}
+
 }
---
+++ app/src/services/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:5289/api'; 
+
+  constructor(private http: HttpClient) { }
+
+  // HTTP GET Request Example
+  getData(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/led/status`);
+  }
+
+  // HTTP POST Request Example
+  postData(payload: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/data`, payload);
+  }
+}
